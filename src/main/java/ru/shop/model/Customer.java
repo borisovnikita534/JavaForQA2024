cd0351a872a99dfe4479ce01bc1d8066cbb32438
a.norin <a.norin@tinkoff.ru>
2024-04-17 09:49:31 +0300

    day2

--- src/main/java/ru/shop/model/Customer.java
+++ src/main/java/ru/shop/model/Customer.java
@@ -1,43 +1,13 @@
 package ru.shop.model;
 
+import lombok.Value;
+
 import java.util.UUID;
 
+@Value
 public class Customer {
-    private final UUID id;
-    private final String name;
-    private final String phone;
-    private final long age;
-
-    public Customer(UUID id, String name, String phone, long age) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Customer{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", phone='" + phone + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public long getAge() {
-        return age;
-    }
+    UUID id;
+    String name;
+    String phone;
+    long age;
 }
--- src/main/java/ru/shop/model/Order.java
+++ src/main/java/ru/shop/model/Order.java
@@ -1,50 +1,14 @@
 package ru.shop.model;
 
+import lombok.Value;
+
 import java.util.UUID;
 
+@Value
 public class Order {
-    private final UUID id;
-    private final UUID customerId;
-    private final UUID productId;
-    private final long count;
-    private final long amount;
-
-    public Order(UUID id, UUID customerId, UUID productId, long count, long amount) {
-        this.id = id;
-        this.customerId = customerId;
-        this.productId = productId;
-        this.count = count;
-        this.amount = amount;
-    }
-
-    @Override
-    public String toString() {
-        return "Order{" +
-                "id=" + id +
-                ", customerId=" + customerId +
-                ", productId=" + productId +
-                ", count=" + count +
-                ", amount=" + amount +
-                '}';
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public UUID getCustomerId() {
-        return customerId;
-    }
-
-    public UUID getProductId() {
-        return productId;
-    }
-
-    public long getCount() {
-        return count;
-    }
-
-    public long getAmount() {
-        return amount;
-    }
+    UUID id;
+    UUID customerId;
+    UUID productId;
+    long count;
+    long amount;
 }
